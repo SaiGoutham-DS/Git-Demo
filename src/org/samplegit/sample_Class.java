633f2e0f0057816006c14b99dc36e2a7563c1822
SaiGoutham-DS <gouthamsai.ds@gmail.com>
2023-02-22 14:44:46 +0530

    added

---
+++ src/org/samplegit/sample_Class.java
@@ -0,0 +1,31 @@
+package org.samplegit;
+
+public class sample_Class {
+	
+	
+	private void bike() {
+		System.out.println("Java");
+
+	}
+	
+	private void model() {
+		System.out.println("42");
+		
+		
+
+	}
+
+	public static void main(String[] args) {
+		
+	
+	
+	sample_Class s = new sample_Class();
+	
+	s.bike();
+	s.model();
+	
+	
+	
+	}
+	
+}
